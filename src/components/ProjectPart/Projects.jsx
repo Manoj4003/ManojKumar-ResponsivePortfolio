4b50727cfaa0f8db94d4317b582ea9e404580e4e
Manoj4003 <manojknr16@gmail.com>
2025-10-06 21:52:06 +0530

    Updated Skills Animations

--- src/components/ProjectPart/Projects.jsx
+++ src/components/ProjectPart/Projects.jsx
@@ -16,7 +16,7 @@ const Projects = () => {
       image: project1,
       description: "A modern, responsive business website showcasing services, portfolio, team, and contact sections.",
       techUsed: ["HTML", "CSS", "JavaScript" ,"Bootstrap"],
-      demo: "https://your-demo-link.com",
+      demo: "https://starupbussines.web.app/",
       code: "https://github.com/Manoj4003/Business_project_with_mobile_responsive_updated_project",
     },
     {
@@ -24,7 +24,7 @@ const Projects = () => {
       image: project2,
       description: "A responsive real estate website template featuring property listings, detail pages, and a contact form.",
       techUsed: ["HTML", "CSS", "JavaScript" ,"Bootstrap"],
-      demo: "https://your-demo-link.com",
+      demo: "https://villa-restaurant-website.netlify.app/",
       code: "https://github.com/Manoj4003/Restaurant-villa-with-mobile-responsive",
     },
     {
@@ -32,7 +32,7 @@ const Projects = () => {
       image: project3,
       description: "A business template with multipage structure, hero carousel, product sections, and a contact form.",
       techUsed: ["HTML", "CSS", "JavaScript" ,"React"],
-      demo: "https://your-demo-link.com",
+      demo: "https://mexant-website-c76cd.web.app/",
       code: "https://github.com/Manoj4003/mexant-website-using-react-with-mobile-view",
     },
     {
@@ -40,7 +40,7 @@ const Projects = () => {
       image: project4,
       description: "A professional website template for business schools with courses, team, and contact sections, optimized for mobile.",
       techUsed: ["HTML","CSS","JavaScript"],
-      demo: "https://your-demo-link.com",
+      demo: "",
       code: "https://github.com/Manoj4003/StartUp-website-with-mobile-responsive",
     },
     {
@@ -48,7 +48,7 @@ const Projects = () => {
       image: project5,
       description: "A sleek personal portfolio showcasing skills, projects, and experience with an interactive design, fully responsive across all devices.",
       techUsed: ["React", "CSS","JavaScript","React"],
-      demo: "https://your-demo-link.com",
+      demo: "",
       code: "https://github.com/Manoj4003/ManojKumar-ResponsivePortfolio",
     },
     {
@@ -56,7 +56,7 @@ const Projects = () => {
       image: project6,
       description: "Personal portfolio hero section built with React.",
       techUsed: ["React", "CSS", "Bootstrap"],
-      demo: "https://your-demo-link.com",
+      demo: "",
       code: "https://github.com/yourusername/todo-app",
     },
   ];
